c68214ce296c7712e0dbe554e8c2e0963f32baee
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-12-02 18:11:24 +0300

    .gitignore dosyası eklendi.

---
+++ InstagramClone/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Büşra Özkan on 2.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
