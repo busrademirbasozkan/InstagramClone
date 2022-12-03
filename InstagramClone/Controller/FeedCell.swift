0efa2c7f9d9587a3be9cd9dc6df4e094b3d007ef
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-12-03 21:22:37 +0300

    Feed arayüzü oluşturuldu.

---
+++ InstagramClone/Controller/FeedCell.swift
@@ -0,0 +1,33 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by Büşra Özkan on 3.12.2022.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var userimageView: UIImageView!
+    @IBOutlet weak var likeLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    @IBAction func likeButton(_ sender: Any) {
+    }
+    
+}
