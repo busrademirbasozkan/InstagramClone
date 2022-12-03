16764ada409f115617a78e632c751fcbfa805057
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-12-03 15:35:17 +0300

    UUID ile resimlerin kaydedilmesi sağlandı.

--- InstagramClone/Controller/UploadViewController.swift
+++ InstagramClone/Controller/UploadViewController.swift
@@ -13,20 +13,20 @@ import FirebaseFirestore
 import FirebaseStorage
 
 class UploadViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentText: UITextField!
     @IBOutlet weak var upload: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         imageView.isUserInteractionEnabled = true
         let gesture = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
         imageView.addGestureRecognizer(gesture)
     }
     
-
+    
     @objc func chooseImage(){
         let picker = UIImagePickerController()
         picker.delegate = self
@@ -46,24 +46,35 @@ class UploadViewController: UIViewController , UIImagePickerControllerDelegate, 
         let storageReference = storage.reference()
         
         let mediaFolder = storageReference.child("media")
-         
+        
         if let data = imageView.image?.jpegData(compressionQuality: 0.5){
-            let imageReference = mediaFolder.child("image.jpg")
+            let uuid = UUID().uuidString
+            let imageReference = mediaFolder.child("\(uuid).jpeg")
             imageReference.putData(data, metadata: nil) { (metadata, error) in
                 if error != nil {
-                    print(error?.localizedDescription)
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
                 }else{
                     imageReference.downloadURL { (url, error) in
                         if error == nil {
                             let imageUrl = url?.absoluteString
-                            print(imageUrl)
+                            
+                            //DATABASE
+                            
                         }
                     }
                 }
             }
         }
+    }
+    
+    
+    func makeAlert(titleInput:String, messageInput:String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
         
     }
     
-
+    
 }
